f57d857609c6d97e3d657eb1aa257afebf955089
Ajayi favour <97252800+fevico@users.noreply.github.com>
2023-10-31 14:52:08 +0100

    Update index.ts
    changes

--- server/src/cloud/index.ts
+++ server/src/cloud/index.ts
@@ -1,11 +1,5 @@
 import { CLOUD_KEY, CLOUD_NAME, CLOUD_SECRET } from '#/utiles/variables';
 import {v2 as cloudinary} from 'cloudinary';
 
-cloudinary.config({
-    cloud_name : CLOUD_NAME,
-    api_key: CLOUD_KEY,
-    api_secret: CLOUD_SECRET,
-    secure: true
-});
 
 export default cloudinary
